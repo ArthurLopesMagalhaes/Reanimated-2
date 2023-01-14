eaf818dcdd64d1cd6b4551e9ac45ad4578bc8fc8
Arthur Lopes Magalhães <arthurlopes462@hotmail.com>
2023-01-14 19:36:39 -0300

    InterpolateColor

--- App.tsx
+++ App.tsx
@@ -2,11 +2,12 @@ import "react-native-gesture-handler";
 import { gestureHandlerRootHOC } from "react-native-gesture-handler";
 
 import { BasicsReanimated } from "./src/BasicsReanimated";
+import { InterpolateColors } from "./src/InterpolateColors";
 import { PanGesture } from "./src/PanGesture";
 import { InterpolateScrollView } from "./src/ScrollView";
 
 function App() {
-  return <InterpolateScrollView />;
+  return <InterpolateColors />;
 }
 
 export default gestureHandlerRootHOC(App);
