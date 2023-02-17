870ad9b487312f13dd209ce77ca3d35ebb349d85
Arthur Lopes Magalhães <arthurlopes462@hotmail.com>
2023-02-17 16:39:44 -0300

    change npm to yarn

--- App.tsx
+++ App.tsx
@@ -1,13 +1,16 @@
 import "react-native-gesture-handler";
 import { gestureHandlerRootHOC } from "react-native-gesture-handler";
+import { AnimatedTabs } from "./src/AnimatedTabs";
 
 import { BasicsReanimated } from "./src/BasicsReanimated";
+import { CircularProgressBar } from "./src/CircularProgressBar";
 import { InterpolateColors } from "./src/InterpolateColors";
 import { PanGesture } from "./src/PanGesture";
+import { PerspectiveMenu } from "./src/PerspectiveMenu";
 import { InterpolateScrollView } from "./src/ScrollView";
 
 function App() {
-  return <InterpolateColors />;
+  return <CircularProgressBar />;
 }
 
 export default gestureHandlerRootHOC(App);
